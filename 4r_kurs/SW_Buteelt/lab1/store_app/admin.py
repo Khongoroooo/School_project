a4f9219432e38efa29234a8274451c4ec5673595
Hongorzul <hongoroo1289810@gmail.com>
2025-09-02 22:04:43 +0800

    using models

---
+++ 4r_kurs/SW_Buteelt/lab1/store_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Angilal,Baraa
+
+admin.site.register(Angilal)
+admin.site.register(Baraa)
---
+++ 4r_kurs/SW_Buteelt/lab1/store_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Angilal (models.Model):
+    aname = models.CharField(max_length=200)
+    def __str__(self):
+        return self.aname
+    
+class Baraa (models.Model):
+    bname = models.CharField(max_length=200)
+    angilal = models.ForeignKey(Angilal,on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.bname}({self.angilal})"
